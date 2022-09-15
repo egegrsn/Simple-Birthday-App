e35f0dfc7a7177713a73a244702ca1ac13c91d8b
Ege Girsen <38487440+egegrsn@users.noreply.github.com>
2022-09-15 04:19:59 +0300

    full commit

---
+++ BirthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayApp
+//
+//  Created by Ege Girsen on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
